61069cd106298407ef8270d60a96bfe49bf4df73
Ahmet Erdogan <329188@student.mboutrecht.nl>
2020-02-13 13:37:15 +0100

    Signup header

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
-<div class="desktop-image">
-<header class="masthead">
-      <div class="intro-text">
-      </div>
-    </div>
-  </header>
+<div class="header-sign">
+  <ul class="navbar-nav text-uppercase ml-auto">
+    <li>
+      <a class="sign-link" href="index.php?content=inloggen">SIGN UP</a>
+    </li>
+  </ul>
+</div>
