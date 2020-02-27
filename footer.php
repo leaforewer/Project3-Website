ce50b7b6e471a68aeee1977071e65fd223e45f68
Ahmet Erdogan <329188@student.mboutrecht.nl>
2020-02-27 17:42:04 +0100

    asd

--- footer.php
+++ footer.php
@@ -4,6 +4,6 @@
 
 <div class="footer1" id="footer">
             <div class="footer-copyright text-center py-3 " id="footer1">© 2019 Copyright:
-                <a id="footer2"> De LEAF</a>
+                <a id="footer2"> De TURKEN</a>
             </div>
         </div>
