e26b1826a88688c5d2281d9e053cdd0f10eae14e
Talha Berk Kiymaz <326753@student.mboutrecht.nl>
2020-04-06 12:39:02 +0200

    game is toegevoegd

---
+++ game.php
@@ -0,0 +1,5 @@
+<div class="col-12">
+
+<iframe src="https://i.simmer.io/@MaxxieMax/testr" style="width:1080px;height:600px"></iframe>
+
+</div>
--- navbar.php
+++ navbar.php
@@ -12,7 +12,7 @@
                         <a class="nav-link" href="./index.php?content=users">Users</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">navbar</a>
+                        <a class="nav-link" href="./index.php?content=game">Game</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">navbar</a>
