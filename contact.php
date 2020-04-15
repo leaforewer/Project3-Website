16186a74881c1cdfafde1ccb426cec83407a4c21
Ahmet Erdogan <329188@student.mboutrecht.nl>
2020-04-15 18:16:47 +0200

    regelingen

--- contact.php
+++ contact.php
@@ -4,17 +4,17 @@
     </div>
     <div class="col-6 abc">
         <form action="./index.php?content=contact_script" method="post">
-
-
+        
+        
             <div class="form-group">
                 <ion-icon style="width: 35px; height: 35px;" name="chatbubbles-outline"></ion-icon> 
                 <label  style="padding-right: 350px; padding-bottom: 5px; font-size: 25px;" for="">Onderwerp
-            </label>
+                </label>
                 <select id="subject" name="subject" style="padding-left: 30px; background-color: #6CCCCE; color: whitesmoke; padding-top: 2px; padding-bottom: 2px;">
                     <option value='feedback'>Feedback</option>
                     <option value ='klacht'>Klachten</option>
                     <option value='probleem'>Problemen</option>
-
+                    
                 </select>
             </div>
             <div class="form-group">
@@ -26,18 +26,18 @@
                 <label style="font-size: 20px;" for="exampleFormControlTextarea1">Tekst</label>
                 <textarea class="form-control" id="exampleFormControlTextarea1" id="message" name="message" rows="3"></textarea>
             </div>
-
-
+            
+            
             <div style="font-size: 14px; padding-left: 15px;">
             </div>
             <small id="Help" class="form-text">*Schrijf hier uw opmerkingen</small>
+        </div>
+        <div class="col-12" style="padding: 15px;">
+            <button type="submit" class="btn btn-primary submit">Versturen</button>
+        </div>
+        
     </div>
-    <div class="col-12" style="padding: 15px;">
-        <button type="submit" class="btn btn-primary submit">Versturen</button>
-    </div>
-    </form>
-</div>
+</form>
 <div class="col-6">
     <img src="./img/registreren1.png" style="width: 450px; height: 450px; " alt="aanmelden">
-</div>
 </div>
