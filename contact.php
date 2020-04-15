1ba5ecdc7e7bec533243931fa81dadd469ce8ac4
Talha Berk Kiymaz <326753@student.mboutrecht.nl>
2020-04-15 17:48:03 +0200

    hahhahaajajjaj

---
+++ contact.php
@@ -0,0 +1,32 @@
+<div class="row">
+    <div class="col-12">
+        <h3>Contact</h3>
+    </div>
+    <div class="col-6 abc">
+        <form action="./index.php?content=aanmelden_script" method="post">
+
+
+            <div class="form-group">
+                <label for="infix">E-mailadres</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+                <small id="Help" class="form-text ">*Verplicht veld</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Tekst</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+
+        
+            <div style="font-size: 14px; padding-left: 15px;">
+            </div>
+            <small id="Help" class="form-text text-muted">*Schrijf hier uw message</small>
+    </div>
+    <div class="col-12" style="padding: 15px;">
+        <button type="submit" class="btn btn-primary submit">Versturen</button>
+    </div>
+    </form>
+</div>
+<div class="col-6">
+    <img src="./img/registreren1.png" style="width: 450px; height: 450px; " alt="aanmelden">
+</div>
+</div>
