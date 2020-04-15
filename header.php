996a9038e69756780502eb2f67c507b06eff9b14
Ahmet Erdogan <329188@student.mboutrecht.nl>
2020-04-15 19:36:42 +0200

    final result

--- header.php
+++ header.php
@@ -1,9 +1,8 @@
 <div class="header-sign">
   <ul class="navbar-nav text-uppercase ml-auto">
     <li class="nav-item">
-      <a class="sign-link " href="index.php?content=aanmelden"><ion-icon style="width: 28px; height:28px;" name="log-in"></ion-icon><p  class = "sign-link" >AANMELDEN</p></a>
+      <a class="sign-link nav-link" href="index.php?content=aanmelden"><ion-icon style="width: 28px; height:28px;" name="log-in"></ion-icon>  AANMELDEN</a>
     </li>
-
   </ul>
 </div>
 <div class="color-bar">
