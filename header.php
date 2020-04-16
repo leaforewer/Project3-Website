dc2a7d343a175f30e30b4651cc71591b2174b728
Ahmet Erdogan <329188@student.mboutrecht.nl>
2020-04-16 08:52:56 +0200

    final

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <div class="header-sign">
   <ul class="navbar-nav text-uppercase ml-auto">
     <li class="nav-item">
-      <a class="sign-link nav-link" href="index.php?content=aanmelden"><ion-icon style="width: 28px; height:28px;" name="log-in"></ion-icon>  AANMELDEN</a>
+      <a class="sign-link nav-link" href="index.php?content=aanmelden">|  AANMELDEN</a>
     </li>
   </ul>
 </div>
@@ -9,3 +9,4 @@
 <img src="#" alt="">
 
 </div>
+
